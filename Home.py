c3bb29af394f06be4d6f2f7283237c9091a7d18f
benoom <benjoom@gmail.com>
2024-06-29 14:23:28 -0400

    first commit

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.write("""
+# Welcome
+The use of this application is intended to provide feedback on whether the pills selected by the user in this application, match the pills laid out for consumption and analyzed via a picture uploaded.
+
+""")
