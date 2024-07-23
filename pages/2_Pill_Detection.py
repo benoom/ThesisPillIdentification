e94950d547781b4cd0add2f59cf67216fde79f60
benoom <benjoom@gmail.com>
2024-07-23 18:56:07 -0400

    commit of the actual app

---
+++ pages/2_Pill_Detection.py
@@ -0,0 +1,231 @@
+import streamlit as st
+import base64
+from PIL import Image
+import cv2
+import numpy as np
+from roboflow import Roboflow
+from collections import Counter
+
+project_url_od, private_api_key, uploaded_file_od, pill_input_array = ("", "", "", [])
+
+if 'project_url_od' not in st.session_state:
+    st.session_state['project_url_od'] = "https://app.roboflow.com/ms-cs-thesis/medication-identification-v3/3"
+if 'workspace_id' not in st.session_state:
+    st.session_state['workspace_id'] = 'ms-cs-thesis'
+if 'model_id' not in st.session_state:
+    st.session_state['model_id'] = 'medication-identification-v3'
+if 'version_number' not in st.session_state:
+    st.session_state['version_number'] = '3'
+if 'confidence_threshold' not in st.session_state:
+    st.session_state['confidence_threshold'] = "90"
+if 'overlap_threshold' not in st.session_state:
+    st.session_state['overlap_threshold'] = "50"
+if 'include_bbox' not in st.session_state:
+    st.session_state['include_bbox'] = "Yes"
+if 'show_class_label' not in st.session_state:
+    st.session_state['show_class_label'] = 'Show Labels'
+if 'box_type' not in st.session_state:
+    st.session_state['box_type'] = "regular"
+if 'private_api_key' not in st.session_state:
+    st.session_state['private_api_key'] = "FoM7U0Xonm0lSnV74Bfl"
+if 'uploaded_file_od' not in st.session_state:
+    st.session_state['uploaded_file_od'] = ""
+if 'pill_input_array' not in st.session_state:
+    st.session_state['pill_input_array'] = []
+if 'box_width' not in st.session_state:
+    st.session_state['box_width'] = "1"
+
+##########
+#### Set up main app logic
+##########
+
+def run_inference(uploaded_img, inferenced_img):
+    rf = Roboflow(api_key=st.session_state['private_api_key'])
+    project = rf.workspace(st.session_state['workspace_id']).project(st.session_state['model_id'])
+    version = project.version(st.session_state['version_number'])
+    model = version.model
+    
+                
+    predictions = model.predict(uploaded_img, overlap=int(st.session_state['overlap_threshold']),
+        confidence=int(st.session_state['confidence_threshold']), stroke=int(st.session_state['box_width']))
+
+    # drawing bounding boxes with the Pillow library
+    # https://docs.roboflow.com/inference/hosted-api#response-object-format
+    collected_predictions = []
+    collected_classes = []
+    
+
+    for bounding_box in predictions:
+        x0 = bounding_box['x'] - bounding_box['width'] / 2
+        x1 = bounding_box['x'] + bounding_box['width'] / 2
+        y0 = bounding_box['y'] - bounding_box['height'] / 2
+        y1 = bounding_box['y'] + bounding_box['height'] / 2
+        class_name = bounding_box['class']
+        confidence_score = bounding_box['confidence']
+        box = (x0, x1, y0, y1)
+        detected_x = int(bounding_box['x'] - bounding_box['width'] / 2)
+        detected_y = int(bounding_box['y'] - bounding_box['height'] / 2)
+        detected_width = int(bounding_box['width'])
+        detected_height = int(bounding_box['height'])
+        # ROI (Region of Interest), or detected bounding box area
+        roi_bbox = [detected_y, detected_height, detected_x, detected_width]
+        collected_classes.append(class_name)
+        collected_predictions.append({"class":class_name, "confidence":confidence_score,
+                                    "x0,x1,y0,y1":[int(x0),int(x1),int(y0),int(y1)],
+                                    "Width":int(bounding_box['width']), "Height":int(bounding_box['height']),
+                                    "ROI, bbox (y+h,x+w)":roi_bbox,
+                                    "bbox area (px)":abs(int(x0)-int(x1))*abs(int(y0)-int(y1))})
+        # position coordinates: start = (x0, y0), end = (x1, y1)
+        # color = RGB-value for bounding box color, (0,0,0) is "black"
+        # thickness = stroke width/thickness of bounding box
+        start_point = (int(x0), int(y0))
+        end_point = (int(x1), int(y1))
+        
+        # draw/place bounding boxes on image
+        cv2.rectangle(inferenced_img, start_point, end_point, color=(0,0,0), thickness=2)
+        
+
+        cv2.putText(inferenced_img,
+            class_name,#text to place on image
+            (int(x0), int(y0) - 5),#location of text
+            fontFace=cv2.FONT_HERSHEY_DUPLEX,#font
+            fontScale=1.5,#font scale
+            color=(255,255,255),#text color
+            thickness=2#thickness/"weight" of text
+            )
+    
+
+    ## Subtitle.
+    st.write("### Inferenced Image")    
+    st.image(inferenced_img, caption="Inferenced Image", use_column_width=True)
+
+        
+    classesCount = Counter(collected_classes)
+    userInputCount = Counter(st.session_state['pill_input_array'])
+    
+    if len(collected_classes) != len(st.session_state['pill_input_array']): 
+        st.write("Number of pills selected does not match the number of pills in the image. Please verify selections")
+        for x in classesCount:
+            if userInputCount.get(x) == None:
+                st.write("Expected 0 " + x + " and detected " + str(classesCount.get(x)) + ". Please put the pill(s) back.")
+            else:
+                if userInputCount.get(x) != classesCount.get(x):
+                    st.write("Expected " + str(userInputCount.get(x)) + " " + x + " and detected " + str(classesCount.get(x)) + ". Please put " + str(abs(userInputCount.get(x)-classesCount.get(x))) + " pill(s) back")
+                else:
+                    st.write("Expected " + str(userInputCount.get(x)) + " " + x + " and detected " + str(classesCount.get(x)))
+        st.write(" ")
+
+    
+    else:
+        flag=0
+        for i in classesCount:
+            if classesCount.get(i) != userInputCount.get(i):
+                st.write("Not a Match")    
+                flag=1
+                break
+        if flag==0:
+            st.write(" ")
+            st.write("The correct pills have been chosen")
+            for x in userInputCount:
+                st.write("Expected " + str(userInputCount.get(x)) + " " + x + " and detected " + str(classesCount.get(x)))
+        else:
+            st.write(" ")
+            st.write("The incorrect pills are chosen. Please try again")
+            for x in userInputCount:
+                st.write("Expected " + str(userInputCount.get(x)) + " " + x + " and detected " + str(classesCount.get(x)))
+
+    st.write(" ")
+    st.write("Pill Images Recognized:")
+    for pillsAnalyzed in collected_classes:
+        st.write(pillsAnalyzed)
+    st.write(" ")
+
+    st.write("Pill Images Selected:")    
+    for pillsSelected in st.session_state['pill_input_array']:
+        st.write(pillsSelected)
+    st.write(" ")    
+
+##########
+##### Set up sidebar.
+##########
+# Add in location to select image.
+with st.sidebar:
+    st.write("#### Select an image to upload.")
+    uploaded_file_od = st.file_uploader("Image File Upload",
+                                        type=["png", "jpg", "jpeg"],
+                                        accept_multiple_files=False)
+
+    
+    #holds the pill and qty of the pill
+    pill_selected = ""
+    pill_qty = ""
+
+    pill_selected = st.selectbox("Pill Selector:",
+                                 options=("8 HR Acetaminophen 650 MG Extended Release Tablet",                                          
+                                          "Acetaminophen 500 MG Tablet",                                          
+                                          "Ibuprofen 200 MG Tablet",
+                                          ),
+                                key="pill_selected")
+    
+    pill_qty = st.number_input("Enter the pill quantity", step=1,min_value=1,value=1)
+
+    if st.button("Add a Pill and Qty"):
+        x = pill_qty
+        while x > 0:
+            st.session_state['pill_input_array'].append(pill_selected)
+            x=x-1
+        pill_selected = ""
+        pill_qty = ""
+
+    if st.button("Clear Pill Entries"):
+        st.session_state["pill_input_array"].clear()
+        
+##########
+##### Set up project access.
+##########
+
+## Title.
+st.write("# Pill Detection")
+
+with st.form("project_access"):
+    project_url_od = "https://app.roboflow.com/ms-cs-thesis/medication-identification-v3/3" 
+    private_api_key = "FoM7U0Xonm0lSnV74Bfl" 
+    submitted = st.form_submit_button("Verify Selected Medication")
+    st.write("*** From the left side bar, upload an image (jpg, jpeg, or png) and select each pill you are scheduled to take and the quantity by using the dropdowns and the Add a Pill and Qty button. Your selections can be cleared by using the Clear Pill Entries button. ***")
+    
+#https://app.roboflow.com/ms-cs-thesis/medication-identification-v3/3
+    if submitted:
+        st.write("Loading results...")
+        extracted_url = project_url_od.split("roboflow.com/")[1]
+        #after split1 ['ms-cs-thesis/medication-identification-v3/3']
+        
+        if "model" in project_url_od.split("roboflow.com/")[1]:
+            st.session_state['workspace_id'] = extracted_url.split("/")[0]
+            st.session_state['model_id'] = extracted_url.split("/")[1]
+            st.session_state['version_number'] = extracted_url.split("/")[3]
+        elif "deploy" in project_url_od.split("roboflow.com/")[1]:
+            st.session_state['workspace_id'] = extracted_url.split("/")[0]
+            st.session_state['model_id'] = extracted_url.split("/")[1]
+            st.session_state['version_number'] = extracted_url.split("/")[3]
+        else:
+            st.session_state['workspace_id'] = "ms-cs-thesis" #extracted_url.split("/")[0] # ms-cs-thesis
+            st.session_state['model_id'] = "medication-identification-v3" #extracted_url.split("/")[1] # medication-identification-v3
+            st.session_state['version_number'] = "3" #extracted_url.split("/")[2] # 3
+        
+        if uploaded_file_od != None:
+        # User-selected image.
+            image = Image.open(uploaded_file_od)
+            uploaded_img = cv2.imdecode(np.frombuffer(uploaded_file_od.read(), np.uint8), 1)
+            #uploaded_img = np.array(image)
+            inferenced_img = np.array(image)
+
+            #file_image = Image.open(uploaded_file_od)
+            #image = file_image.resize((640,640))
+            #uploaded_img = np.array(file_image)
+            #image = cv2.imread(uploaded_file_od.name)
+            
+            #uploaded_img = np.asarray()
+            #inferenced_img = uploaded_img.copy()
+
+            run_inference(uploaded_img, inferenced_img)
+
